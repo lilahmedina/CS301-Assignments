414a3d9bc134fe2babaf25b8b0779700022f9413
Delilah Medina <67711334+lilahmedina@users.noreply.github.com>
2024-10-01 06:05:48 +0000

    Implemented some constructors.

--- A2-ChunkList/ChunkList.cpp
+++ A2-ChunkList/ChunkList.cpp
@@ -5,3 +5,46 @@
 #include "ChunkList.h"
 
 
+
+template <class T>
+inline ChunkList<T>::ChunkList()
+{
+    head = nullptr
+    tail = nullptr;
+    listLen = 0;
+    iterNode = nullptr;
+    arrPos= 0;
+    numChunks= 0;
+}
+
+template <class T>
+inline ChunkList<T>::ChunkList(T arr[], int arrLen)
+{
+    head = nullptr
+    tail = nullptr;
+    listLen = 0;
+    numChunks= 0;
+
+}
+
+template <class T>
+inline int ChunkList<T>::GetLength()
+{
+    return listLen;
+}
+
+template <class T>
+inline double ChunkList<T>::LoadFactor()
+{
+    if(head == nullptr){
+        throw EmptyList();
+    }
+    
+}
+
+template <class T>
+inline int ChunkList<T>::ResetIterator()
+{
+   arrPos = 0;
+    iterNode = nullptr;
+}
